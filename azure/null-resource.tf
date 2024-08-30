9acc1eb292ad03408ee5641aefb1e43fab572a80
krosis <xavierivan2000@hotmail.com>
2024-08-30 12:55:20 -0500

    add module folder structure for testing. add destroy script

---
+++ azure/null-resource.tf
@@ -0,0 +1,24 @@
+resource "time_sleep" "wait_and_provision" {
+  depends_on      = [azurerm_linux_virtual_machine.linuxvm]
+  create_duration = "10s"
+
+}
+
+resource "null_resource" "sync_app1_static" {
+  depends_on = [time_sleep.wait_and_provision]
+  for_each   = var.az_enviroment
+  connection {
+    type        = "ssh"
+    host        = azurerm_linux_virtual_machine.linuxvm[each.key].public_ip_address
+    user        = azurerm_linux_virtual_machine.linuxvm[each.key].admin_username
+    private_key = file("~/.ssh/tf-tests")
+  }
+  provisioner "file" {
+    source      = "./dependencies/apache.txt"
+    destination = "/tmp/apache.txt"
+    # on_failure = continue
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo cp /tmp/apache.txt /var/www/html/apache.txt"]
+  }
+}
