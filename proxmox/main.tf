5fdc7e30b72bb7c3ad2cd27dd4cb69c6455f390a
krosis <xavierivan2000@hotmail.com>
2024-10-16 21:40:15 -0500

    proxmox template setup

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -1,78 +1,71 @@
-resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = var.pve_target_node
-  url          = var.vm_image_url
-}
 
-
-resource "proxmox_virtual_environment_vm" "vm_cloudimage_demo" {
-    name = "corintia-demo"
-    description = "test deployment based on cloud_image"
-    vm_id = 2000
-    node_name = var.pve_target_node
-    started = false # start after creation
-    tags = ["terraform", "dev" ]
-
-
-    agent {
-      enabled = false #dont qemu agent
-    }
-
- # if agent is not enabled, the VM may not be able to shutdown properly, and may need to be forced off
+# Ubuntu server template
+resource "proxmox_virtual_environment_vm" "ubuntu_template" {
+  name        = "ubuntu-template"
+  description = "ubuntu 2204 template for k3s and k8s deployments"
+  vm_id       = 5000
+  node_name   = var.pve_target_node
+  template    = true
+  started     = false # start after creation
+  tags        = ["terraform", "template"]
+  agent {
+    enabled = false #dont qemu agent
+  }
+  # if agent is not enabled, the VM may not be able to shutdown properly, and may need to be forced off
   stop_on_destroy = true
-
-    initialization {
-      datastore_id = "local-zfs"
-      user_account {
-        username = "ubuntu"
-        password = "calidad18!"
-        keys = [trimspace(tls_private_key.ubuntu_vm_key.public_key_openssh)]
-      }
-
-      ip_config {
-        ipv4 {
-          address = "dhcp"
-        }
-      }
-
-      # user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
-    }
-
-    network_device {
-      bridge = "vmbr0"
-    }
-    operating_system {
+  # CPU & motherboard
+  bios          = "seabios"
+  scsi_hardware = "virtio-scsi-pci"
+  cpu {
+    architecture = "x86_64"
+    cores        = 2
+    type         = "host"
+  }
+  operating_system {
     type = "l26"
   }
-
+  #RAM
   memory {
-    dedicated = 1024
+    dedicated = 2048
+    floating  = 0
   }
-
-    disk {
-      datastore_id = "local-zfs"
-      # file_format = "qcow2"
-      file_id = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
-      interface = "virtio0"
-      iothread = true
-      discard = "on"
-      size = 10
+  #network interface
+  network_device {
+    enabled = true
+    bridge  = "vmbr0"
+    model   = "virtio"
+  }
+  disk {
+    datastore_id = "local-zfs"
+    file_id      = var.vm_image_local
+    file_format  = "qcow2"
+    interface    = "scsi0"
+    iothread     = false
+    discard      = "ignore"
+    size         = 10   #Gigabytes
+    ssd          = true #ssd emulation
+  }
+  # cloudinit setup
+  initialization {
+    datastore_id = "local-zfs"
+    user_account {
+      username = var.vm_user
+      password = var.vm_passwd
+      keys     = [var.vm_ssh_key]
     }
-
-    serial_device {}
-}
-
-resource "tls_private_key" "ubuntu_vm_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-output "ubuntu_vm_private_key" {
-  value     = tls_private_key.ubuntu_vm_key.private_key_pem
-  sensitive = true
-}
-
-output "ubuntu_vm_public_key" {
-  value = tls_private_key.ubuntu_vm_key.public_key_openssh
+    # change ip after cloning this resource
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+  # video signal
+  vga {
+    type = "serial0" #allows for connectivity even without network access
+  }
+  #serial
+  serial_device {
+    device = "socket"
+  }
 }
--- proxmox/variables.tf
+++ proxmox/variables.tf
@@ -1,28 +1,45 @@
 variable "pve_api_endpoint" {
-    type = string
-    description = "URL del enpdpoint usado para comunicarse con la API de Proxmox"
-    default="https://10.10.0.110:8006/"
+  type        = string
+  description = "URL del enpdpoint usado para comunicarse con la API de Proxmox"
+  default     = ""
 }
 
 variable "pve_api_token" {
-    type = string
-    description = "API Token requerido para autenticarse contra Proxmox"
-    default="terraform@pve!terraform=e6d4d9d2-4f53-4ad2-beb4-da593ce2ef47"
+  type        = string
+  description = "API Token requerido para autenticarse contra Proxmox"
+  default     = ""
 }
 
 variable "pve_target_node" {
-    type = string
-    description = "Nodo principal de Proxmox"
-    default="citadel"
+  type        = string
+  description = "Nodo principal de Proxmox"
+  default     = ""
 }
 
-variable "vm_ssh_key" {
-    description = "llave SSH de conexion contra las VMs"
-    default="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHTOuTHR+Dh+dMdPkpmE1NkbUBnqgun+9aOA9lhQ/5ET terraform-dev-ssh"
-}
 
 variable "vm_image_url" {
-    type = string
-    description = "URL de proveedor de la imagen de la VM. formatos: .img y .qcow2"
-    default="https://cloud-images.ubuntu.com/oracular/20240611/oracular-server-cloudimg-amd64.img"
+  type        = string
+  description = "URL de proveedor de la imagen de la VM. formatos: .img y .qcow2"
+  default     = ""
+}
+
+variable "vm_image_local" {
+  type        = string
+  description = "downloaded image path inside proxmox"
+  default     = ""
+}
+
+variable "vm_user" {
+  type        = string
+  description = "username for cloudinit setup"
+  default     = ""
+}
+variable "vm_passwd" {
+  type        = string
+  description = "password for cloudinit setup"
+  default     = ""
+}
+variable "vm_ssh_key" {
+  description = "llave SSH de conexion contra las VMs"
+  default     = ""
 }
